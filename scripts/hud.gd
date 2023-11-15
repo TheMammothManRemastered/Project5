277f1eec5cc0e4a245bfa6ba79cb97bcf760ac02
TheMammothManRemastered <54153096+TheMammothManRemastered@users.noreply.github.com>
2023-11-15 15:07:20 -0500

    hotfix for the HUD

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -2,11 +2,9 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Label.text = "Currently Facing: " + ("LEFT" if (LevelManager.player.curr_facing == -1) else "RIGHT")
+	$MarginContainer/Label.text = "Currently Facing: " + ("LEFT" if (LevelManager.player.curr_facing == -1) else "RIGHT")
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Label.text = "Currently Facing: " + ("LEFT" if (LevelManager.player.curr_facing == -1) else "RIGHT")
-	$Label2.text = "Parry Charged: " + ("NO" if LevelManager.player.is_parrying else "YES")
-	$Label4.text = "Health :" + str(LevelManager.player.health)
+	$MarginaContainer/Label.text = "Currently Facing: " + ("LEFT" if (LevelManager.player.curr_facing == -1) else "RIGHT") + "\nParry Charged: " + ("NO" if LevelManager.player.is_parrying else "YES") + "\nHealth :" + str(LevelManager.player.health)
